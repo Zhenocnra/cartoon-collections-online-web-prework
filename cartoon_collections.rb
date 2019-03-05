5133abfa2b374b6075286e78840c19baa2711000
Zoncharne Lee <zoncharne@gmail.com>
2019-03-05 08:46:46 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,9 @@ def roll_call_dwarves(names)
 end
 
 def summon_captain_planet(planeteers)
-  
-  
+  planeteers.collect do |call|
+    call.capitalize << "!"
+  end
 end
 
 def long_planeteer_calls# code an argument here
